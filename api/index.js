8bf301554168181fb60cc660ae335df5a017990e
Snowrk <shinsan555555@gmail.com>
2024-11-14 19:36:58 +0530

    going back

--- api/index.js
+++ api/index.js
@@ -99,7 +99,7 @@ async function run() {
         response.send({ err: e });
       }
     });
-    app.get("/login", async (request, response) => {
+    app.post("/login", async (request, response) => {
       try {
         const { email, password } = request.body;
         const match = await users.findOne({ email: email });
